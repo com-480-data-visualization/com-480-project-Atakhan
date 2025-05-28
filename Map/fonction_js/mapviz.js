e83314ff383f171358cc7eef2b547ce790d0743d
Scottkushy <mael.tournier@epfl.ch>
2025-05-28 12:24:31 +0200

    Update mapviz.js

--- Map/fonction_js/mapviz.js
+++ Map/fonction_js/mapviz.js
@@ -4,7 +4,7 @@ const stats = {
   dragon: "Dragon\nHP: 3500\nRespawn: 5 min\nBuff: Elemental Soul",
   herald: "Rift Herald\nHP: 8000\nFirst spawn: 8:00\nDrops: Eye of Herald",
   minion: "Minion\nHP: 450–1000\nGold: 14–60\nSpawns every 30s",
-  tower: "Tower\nHP: 5000–5500\nArmor: +40 early\nGold: 250 team gold + 150 local gold\nFirst Tower: +400 bonus gold"
+  tower: "Tower\nHP: 5000–5500\nArmor: +40 early\nGold: 250 team gold\nFirst Tower: +400 bonus gold"
 };
 
 const monsters = [
